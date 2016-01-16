4b44d315b98a921557a1abbba775798b936cb7af
fanzl <fzl1591564@163.com>
2016-01-16 22:34:45 +0800

    AVR Car project created

---
+++ AVRCarDriverApplication/AVRCarDriverApplication.c
@@ -0,0 +1,17 @@
+/*
+ * AVRCarDriverApplication.c
+ *
+ * Created: 2016/1/16 22:21:43
+ *  Author: fanzl
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
---
+++ AVRCarDriverApplication/LCD1602Drive.h
@@ -0,0 +1,20 @@
+/*
+ * LCD1602Drive.h
+ *
+ * Created: 2016/1/16 22:26:23
+ *  Author: fanzl
+ */ 
+
+
+#ifndef LCD1602DRIVE_H_
+#define LCD1602DRIVE_H_
+
+#include <avr/io.h>
+#include <delay.h>
+
+
+
+
+
+
+#endif /* LCD1602DRIVE_H_ */
